219f89febb9ea65d3368ab10091fde011e76b24a
Juanjo <juanjo@jmontiel.es>
2021-02-28 15:00:37 +0100

    Agregadas historias y una tabla para representarlas.

---
+++ StoriesLibrary/StoriesLibrary/Shared/Story.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoriesLibrary.Shared
+{
+	public class Story
+	{
+
+		public string Title { get; set; }
+
+		public string Author { get; set; }
+
+		public DateTime PublishedDate { get; set; }
+
+		public string Category { get; set; }
+
+	}
+}
