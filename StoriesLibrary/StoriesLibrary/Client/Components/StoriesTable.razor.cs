8710d3398f47ebb7d8181d1f25e4b394fbbd8438
Juanjo <juanjo@jmontiel.es>
2021-03-01 07:29:33 +0100

    Creo el componente StoriesTable para representar cualquier tabla con historias dentro de nuestra web. Añado parámetros al componente para pasarle las historias, y también el caption de la tabla.

---
+++ StoriesLibrary/StoriesLibrary/Client/Components/StoriesTable.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using StoriesLibrary.Shared;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoriesLibrary.Client.Components
+{
+	public partial class StoriesTable
+	{
+
+		[Parameter]
+		public List<Story> Stories { get; set; }
+
+		[Parameter]
+		public string Caption { get; set; }
+
+	}
+}
