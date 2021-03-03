8802c5403a27115e14388e039df97f696de53de2
Juanjo <juanjo@jmontiel.es>
2021-03-03 23:43:20 +0100

    Añado un parámetro en cascada que se define en la plantilla y se utiliza en la página principal.

---
+++ StoriesLibrary/StoriesLibrary/Client/Pages/Index.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoriesLibrary.Client.Pages
+{
+	public partial class Index
+	{
+
+		[CascadingParameter(Name = "numberOfStories")]
+		private int numberOfStories { get; set; }
+
+	}
+}
