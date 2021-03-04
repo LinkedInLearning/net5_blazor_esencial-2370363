fc880d4a4e75ea1d64390c474d06b858a2b6bec7
Juanjo <juanjo@jmontiel.es>
2021-03-04 08:16:18 +0100

    Añado un pequeño sistema de búsqueda a nuestra tabla de historias.

--- StoriesLibrary/StoriesLibrary/Client/Components/StoriesTable.razor.cs
+++ StoriesLibrary/StoriesLibrary/Client/Components/StoriesTable.razor.cs
@@ -12,11 +12,45 @@ namespace StoriesLibrary.Client.Components
 	public partial class StoriesTable
 	{
 
+		private List<string> recentSearches = new List<string> { "terror", "dientes", "gato", "perro", "comedia" };
+
+		private string searchField;
+
+		private string textToFilter;
+
+		private List<Story> filteredResults;
+
+		private string captionWithFilter => Caption + (!string.IsNullOrWhiteSpace(textToFilter) ? $". Filtrando por el texto \"{textToFilter}." : "");
+
 		[Parameter]
 		public List<Story> Stories { get; set; }
 
 		[Parameter]
 		public string Caption { get; set; }
 
+		protected override void OnParametersSet()
+		{
+			filteredResults = Stories;
+		}
+
+		private void PerformSearch()
+		{
+			textToFilter = searchField;
+			if (string.IsNullOrWhiteSpace(textToFilter))
+			{
+				filteredResults = Stories;
+			}
+			else
+			{
+				filteredResults = Stories.Where(s => s.Title.Contains(textToFilter, StringComparison.InvariantCultureIgnoreCase)
+					|| s.Author.Contains(textToFilter, StringComparison.InvariantCultureIgnoreCase)
+					|| s.Category.Contains(textToFilter, StringComparison.InvariantCultureIgnoreCase)).ToList();
+			}
+		}
+
+		private void SetRecentSearch(string text)
+		{
+			searchField = text;
+		}
 	}
 }
