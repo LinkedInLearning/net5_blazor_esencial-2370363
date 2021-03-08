aed616bf491bd8366b44544c9f0e67ebc20c617b
Juanjo <juanjo@jmontiel.es>
2021-03-08 01:51:32 +0100

    Hago que PageFooter admita contenido entro de su etiqueta usando ChildContent.

---
+++ StoriesLibrary/StoriesLibrary/Client/Shared/PageFooter.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoriesLibrary.Client.Shared
+{
+	public partial class PageFooter
+	{
+
+		[Parameter]
+		public RenderFragment ChildContent { get; set; }
+	}
+}
