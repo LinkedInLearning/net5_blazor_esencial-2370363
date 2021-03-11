0a7c28fa3fb867e8dd2ff03ffcdfaab4a6e6458d
Juanjo <juanjo@jmontiel.es>
2021-03-12 00:04:39 +0100

    Cambio todo el procesado de response, REadAsStringAsyncm, por la extensión GetFromJsonAsync del paquete System.Net.Http.Json. Mucho más simple.

--- StoriesLibrary/StoriesLibrary/Client/Services/StoriesService.cs
+++ StoriesLibrary/StoriesLibrary/Client/Services/StoriesService.cs
@@ -5,6 +5,7 @@ using StoriesLibrary.Shared;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -33,26 +34,7 @@ namespace StoriesLibrary.Client.Services
 
 		public Task<List<Story>> GetAllAsync() => GetStoriesListAsync("api/stories/");
 
-		private async Task<List<Story>> GetStoriesListAsync(string apiUrl)
-		{
-			if (apiUrl is null)
-			{
-				throw new ArgumentNullException(nameof(apiUrl));
-			}
-
-			try
-			{
-				var response = await httpClient.GetAsync(apiUrl);
-				response.EnsureSuccessStatusCode();
-				var content = await response.Content.ReadAsStringAsync();
-				var stories = JsonSerializer.Deserialize<List<Story>>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-				return stories;
-			}
-			catch (Exception ex)
-			{
-				logger.LogError(ex, "error when retrieving stories from API.");
-				throw ex;
-			}
-		}
+		private Task<List<Story>> GetStoriesListAsync(string apiUrl) =>
+			httpClient.GetFromJsonAsync < List<Story>>(apiUrl);
 	}
 }
